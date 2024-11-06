277633af0e0182f224539ffebacd6a97f5b31726
Dertybus <zlobint696@gmail.com>
2024-11-06 11:45:20 +0400

    Update Task3.js

--- pak/Task3.js
+++ pak/Task3.js
@@ -10,39 +10,39 @@ function randomArray (count, na, m) {
 }
 console.log(randomArray(100, 0, 100));
 //task2
-const arrfg = [];
-const ne = 5;
+const scobki = [];
+const chislo = 5;
 
-for (let i = 1; i < ne + 1; i++) {
-    arrfg.push(i);
+for (let i = 1; i < chislo + 1; i++) {
+    scobki.push(i);
 }
 
-for (let i = 0; i < ne; i++) {
-    const r = Math.floor(Math.random() * ne);
-    [arrfg[i], arrfg[r]] = [arrfg[r], arrfg[i]];
+for (let i = 0; i < chislo; i++) {
+    const r = Math.floor(Math.random() * chislo);
+    [scobki[i], scobki[r]] = [scobki[r], scobki[i]];
 }
 
 console.log(arrfg, fng);
 //task3
-const arreo = [];
-const ngl = 7;
-const fng = 3
+const area = [];
+const testnum = 7;
+const testnum2 = 3
 
-for (let i = 1; i < ngl + 1; i++) {
-    arreo.push(i);
+for (let i = 1; i < testnum + 1; i++) {
+    area.push(i);
 }
 
-for (let i = 0; i < ngl; i++) {21
-    const ra = Math.floor(Math.random() * ngl);
-    [arreo[i], arreo[ra]] = [arreo[ra], arreo[i]];
+for (let i = 0; i < testnum; i++) {21
+    const ra = Math.floor(Math.random() * testnum);
+    [area[i], area[ra]] = [area[ra], area[i]];
 }
-console.log(arreo);
-console.log(arreo.indexOf(3));
+console.log(area);
+console.log(area.indexOf(3));
 //task4
-const arra1 = [2, 2, 17, 21, 45, 12, 54, 31, 53];
+const area1 = [2, 2, 17, 21, 45, 12, 54, 31, 53];
 
-const arra2 = [12, 44, 23, 5];
+const area2 = [12, 44, 23, 5];
 
-const arra3 = arra1.concat(arra2);
+const arra3 = area1.concat(area2);
 
-console.log(arra3)
+console.log(area3)
